94034678031347d83b616c36c9a4d631ac1a0b53
namu00 <skagns3571@naver.com>
2023-03-22 21:10:00 +0900

    clock counter

--- 02_CLK_divider/clk_div.v
+++ 02_CLK_divider/clk_div.v
@@ -12,17 +12,16 @@ module clk_div #(
 );
     localparam SIZE = $clog2(DIV_BY);
 
-    reg [SIZE -1:0] clk_counter;
-    reg clk_out;
+    reg [SIZE -1:0] clk_cnt; //just binary counter
 
-    assign div_clk = clk_out[SIZE -1];
+    assign div_clk = clk_cnt[SIZE -1];
     always @(posedge clk or negedge n_rst)begin
         if(!n_rst) begin
-            clk_counter <= {SIZE{1'b0}};
+            clk_cnt <= {SIZE{1'b0}};
         end
 
         else begin
-            clk_counter <= clk_counter + {{{SIZE-1}{1'b0}},1'b1};
+            clk_cnt <= clk_cnt + {{{SIZE-1}{1'b0}},1'b1};
         end
     end
 endmodule
